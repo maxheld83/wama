28568d00ca05c6b11003f874ff164ac69269902f
Maximilian Held <info@maxheld.de>
2021-07-16 21:05:15 +0200

    add placeholder app

--- R/shiny.R
+++ R/shiny.R
@@ -1,3 +1,14 @@
 #' Placeholder function
 #' @export
 foo <- function() TRUE
+
+#' Placeholder app for testing
+#' @export
+wama_app <- function() {
+  shiny::shinyApp(
+    # change string here to see updates live preview
+    ui = shiny::fluidPage("Hello, world!"),
+    # change string here to see updates shiny backend console
+    server = function(input, output, session) print("Hello, server!")
+  )
+}
